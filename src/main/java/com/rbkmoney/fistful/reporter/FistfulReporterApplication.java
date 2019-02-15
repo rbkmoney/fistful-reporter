1579e9b1c6872e23f2f0bb2502d54894906ec20a
a.karlov <a.karlov@rbkmoney.com>
2019-02-15 12:56:04 +0300

    asd

--- src/main/java/com/rbkmoney/fistful/reporter/FistfulReporterApplication.java
+++ src/main/java/com/rbkmoney/fistful/reporter/FistfulReporterApplication.java
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(scanBasePackages = {"com.rbkmoney.fistful.reporter"})
 @EnableScheduling
 public class FistfulReporterApplication {
-
+    /*asdasd*/
     public static void main(String[] args) {
         SpringApplication.run(FistfulReporterApplication.class, args);
     }
